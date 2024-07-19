3272f51dda96ee1fa176d1c437c6d6e8e1509a9c
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-19 14:46:31 +0530

    Practiced some basic functions of string

---
+++ Strings/StringMethods.js
@@ -0,0 +1,13 @@
+let my_name = "Sunaina Anand";
+
+// length
+lengthofStr = my_name.length
+console.log(lengthofStr);
+
+// touppercase
+uppercaseStr = my_name.toUpperCase();
+console.log(uppercaseStr);
+
+//tolowercase
+ lowercaseStr = my_name.toLowerCase();
+ console.log(lowercaseStr);
