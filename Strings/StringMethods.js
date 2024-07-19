b0213bd543703a5148169b2df42cb85c35d55d82
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-19 15:11:27 +0530

    Practiced some more functions of string

--- Strings/StringMethods.js
+++ Strings/StringMethods.js
@@ -11,3 +11,21 @@ console.log(uppercaseStr);
 //tolowercase
  lowercaseStr = my_name.toLowerCase();
  console.log(lowercaseStr);
+
+ // slice 
+
+ console.log(my_name.slice(2,8));
+ console.log(my_name.replace("Sunaina","naina"));
+
+ let str1 = "    Hello    ";
+ console.log(str1);
+ console.log(str1.trim());
+
+ let str2 = "Hello World!!!"
+
+ for(let i=0 ; i < str2.length ; i++ ){
+     console.log(str2[i]);
+    // console.log(i);
+
+ }
+
---
+++ Strings/traversingfor.js
@@ -0,0 +1,7 @@
+let str1 = "Hello World!!!"
+
+ for(let i=0 ; i < str1.length ; i++ ){
+     console.log(str1[i]);
+    // console.log(i);
+
+ }
