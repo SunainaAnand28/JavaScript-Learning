bee966a9fd4a1db2e7fde484fd7e7505a78ef8d8
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-20 19:56:26 +0530

    Practiced some basics of array

---
+++ Arrays/array.js
@@ -0,0 +1,22 @@
+let a = [1,2,3,4];
+console.log(a);
+
+let b = [1,2,false, null, "not given"];
+console.log(a[0]);
+console.log(b[1]);
+console.log(b[2]);
+console.log(b[3]);
+
+console.log(a.length)
+console.log(b.length)
+
+// changing the value in existing array
+
+a[0]= 5;
+console.log(a);
+
+// Adding a new value in existing array
+
+ a[4]= 7;
+ a[5]= 7;
+ console.log(a);
