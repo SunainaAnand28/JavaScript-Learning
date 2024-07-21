310f404439206609d3ca0894441541098631275f
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-21 20:11:16 +0530

    Practiced pop method.

---
+++ Arrays/pop.js
@@ -0,0 +1,5 @@
+a = [2,3,4,5,6];
+console.log(a);  // log the initialized array
+
+let removedElement = a.pop(); // pop removes the last element from the array and changes the length of array
+console.log(a) // log the new array
