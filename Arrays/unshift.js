db45d255ac696aaacf6cc1eaeb7ca63bb56a1bb5
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-07-21 20:12:49 +0530

    Practiced unshift method.

---
+++ Arrays/unshift.js
@@ -0,0 +1,5 @@
+arr = ['banana', 'apple', 'orange'];
+
+console.log(arr);
+console.log(arr.unshift('apple','grapes')); // used to add one or more than one element at the beginning of the array.
+console.log(arr);
