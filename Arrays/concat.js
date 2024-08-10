1afa305d3774aa570e3ee9990302a8489777e317
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-08-10 13:28:07 +0530

    Practiced concat array method

---
+++ Arrays/concat.js
@@ -0,0 +1,12 @@
+let numarr1 = [12,34,56,77,8,9,97,65434,4345,0];
+let numarr2 = [22,33,44,55,66,77,88,99];
+let numarr3 = [21,32,43,54,65,76,87];
+
+let concatarr1 = numarr1.concat(numarr2);
+console.log(concatarr1)
+
+let concatarr2 = numarr1.concat(numarr1);
+console.log(concatarr2)
+
+let concatarr3 = numarr1.concat(numarr2,numarr3);
+console.log(concatarr3)
