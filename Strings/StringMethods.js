da24d3f07d2855379ce75e2a887016ef7ac97d21
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-08-31 23:19:08 +0530

    Modified the String methods file

--- Strings/StringMethods.js
+++ Strings/StringMethods.js
@@ -2,30 +2,29 @@ let my_name = "Sunaina Anand";
 
 // length
 lengthofStr = my_name.length
-console.log(lengthofStr);
+console.log('Length of string is ',lengthofStr);
 
+console.log("____________________________")
 // touppercase
 uppercaseStr = my_name.toUpperCase();
-console.log(uppercaseStr);
-
+console.log('String in uppercase ',uppercaseStr);
+ 
+console.log("____________________________")
 //tolowercase
- lowercaseStr = my_name.toLowerCase();
- console.log(lowercaseStr);
+lowercaseStr = my_name.toLowerCase();
+console.log('String in lowercase ',lowercaseStr);
 
- // slice 
+// slice 
 
- console.log(my_name.slice(2,8));
- console.log(my_name.replace("Sunaina","naina"));
+console.log("____________________________")
+console.log("Sliced string is ",my_name.slice(2,8));
+
+console.log("____________________________")
+
+console.log('Replaced string is ',my_name.replace("Sunaina","naina"));
 
  let str1 = "    Hello    ";
  console.log(str1);
  console.log(str1.trim());
 
- let str2 = "Hello World!!!"
-
- for(let i=0 ; i < str2.length ; i++ ){
-     console.log(str2[i]);
-    // console.log(i);
-
- }
-
+ 
